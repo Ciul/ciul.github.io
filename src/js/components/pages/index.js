b9e4ae61f253f18ea3863982c332d490e9aad08e
Luis Reactor <luis.osorio@reactor.cl>
2017-07-24 17:23:19 -0400

    Restarted project

--- src/js/components/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Home from './home'
-import Blog from './blog/index'
-import NotFound from './notfound'
-
-export default {
-    Home,
-    Blog,
-    NotFound
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,118 +1,52 @@
-const webpack = require('webpack')
-const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path                  = require('path')
+const webpack               = require('webpack')
+const WebpackChunkHash      = require('webpack-chunk-hash')
+const ChunkManifestPlugin   = require('chunk-manifest-webpack-plugin')
+const HTMLWebpackPlugin     = require('html-webpack-plugin')
+const Config                = require('./package.json')
 
-// Environment
-const production = process.env.NODE_ENV === 'production'
-
-// Entry
-const entry = production
-    ?   [
-            './src/js/client.js'
-    ]
-    :   [
-            './src/js/client.js',
-            'webpack/hot/dev-server.js',
-            'webpack-dev-server/client?http://localhost:8080'
-    ]
-
-// Plugins
-let plugins = production
-    ?   [
-            new webpack.optimize.OccurrenceOrderPlugin(),
-            new webpack.optimize.UglifyJsPlugin(),
-            new webpack.DefinePlugin({
-                'process.env': {
-                    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-                }
-            }),
-            new ExtractTextPlugin({
-                filename: 'style-[contenthash:12].css'
-            }),
-            new HtmlWebpackPlugin({
-                title: 'Luis Carlos Osorio Jayk',
-                filename: 'index.html',
-                template: 'index-template.html'
-            })
-    ]
-    :   [
-            new webpack.NamedModulesPlugin(),
-            new webpack.DefinePlugin({
-                'process.env': {
-                    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-                }
-            }),
-            new ExtractTextPlugin({
-                disable: true
-            }),
-            new webpack.HotModuleReplacementPlugin()
-    ]
-
-// Output
-let output = production
-    ?   {
-            path: path.resolve(__dirname, 'docs'),
-            filename: 'bundle.[hash].min.js',
-            publicPath: '/'
-    }
-    :   {
-            path: path.resolve(__dirname, 'docs'),
-            filename: 'bundle.js',
-            publicPath: '/docs/'
-    }
-
-// Devtool
-let devtool = production
-    ? 'source-map'
-    : 'inline-sourcemap'
-
-// Composed webpack configuration
-let config = {
-    devtool: devtool,
-    entry: entry,
-    output: output,
-    plugins: plugins,
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        path:           path.resolve(__dirname, 'docs'),
+        filename:       '[name]-[chunkhash].js',
+        chunkFilename:  '[name]-[chunkhash].js'
+    },
     module: {
         rules: [
-            
-
             {
-                test: /\.(js|jsx)$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            babelrc: false,
-                            presets: ['env', 'es2015', 'stage-0', 'react'],
-                            plugins: ['transform-class-properties', 'transform-decorators']
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: '[hash:12].[ext]'
-                }
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader'
-                })
+                loader: 'babel-loader'
             }
-
-
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name:       'vendor',
+            minChunks: (module) => {
+                return module.resource && (/node_modules/).test(module.resource)
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:       'manifest',
+            minChunks:  Infinity,
+            filename:   'manifest.js'
+        }),
+        new webpack.HashedModuleIdsPlugin(),
+        new WebpackChunkHash(),
+        new ChunkManifestPlugin({
+            filename:           'manifest.json',
+            manifestVariable:   'webpackManifest',
+            inlineManifest:     true
+        }),
+        new HTMLWebpackPlugin({
+            title:      Config.name,
+            filename:   'index.html',
+            template:   'index-template.ejs'
+        })
+    ]
 }
-
-// console.log(config)
-
-module.exports = config
