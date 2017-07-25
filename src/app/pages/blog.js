08a75f03da279ac3d6880c2b5dc57eef330703e9
Luis Reactor <luis.osorio@reactor.cl>
2017-07-25 08:47:15 -0400

    Simplified structure

---
+++ src/app/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Blog = () => {
+
+    return (
+        <div>
+            <div>Este es el Blog</div>
+            <Link to='/home'>Home</Link>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
 
-import storeFactory from './app/store'
 import App from './app'
-
-const initialState  = {}
-const store         = storeFactory(initialState)
-
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById('app')
 )
